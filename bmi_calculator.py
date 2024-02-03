6e8a24193eec5d9bd1f88977c78f17a883144d12
chachrag <158768780+chachrag@users.noreply.github.com>
2024-02-03 10:18:17 -0800

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -22,7 +22,7 @@ submit_height_button = st.button('Submit Height', on_click=click_button)
 
 if st.session_state.clicked:
     # height_m = calc_height_m(ft_selector, in_selector)
-    height_m = (h_ft*12 + h_in)*2.54/100
+    height_m = (ft_selector*12 + in_selector)*2.54/100
     # (ft_selector*12 + in_selector)*2.54/100
     st.markdown("***")
     st.write('**Enter weight**')
