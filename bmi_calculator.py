b383764d53f63946bf6c8224f52ff4d7bc5be924
chachrag <158768780+chachrag@users.noreply.github.com>
2024-02-03 15:12:53 -0800

    added title

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 
+st.title('Body Mass Index Advisor for Indians')
 
 st.write('**Enter height**')
 ft_selector = st.selectbox('ft', options=[0, 1, 2, 3, 4, 5, 6, 7, 8])
