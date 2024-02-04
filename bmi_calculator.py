93e8febbe4cc1f360fcd72657d5d60ced31e6d24
chachrag <158768780+chachrag@users.noreply.github.com>
2024-02-03 17:14:54 -0800

    added developer name

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,16 +1,6 @@
 import streamlit as st
 import numpy as np
 
-st.title('Body Mass Index Advisor for Indians 🇮🇳')
-st.write('Traditional BMI ranges do not apply to people of Indian ethnicity. This calculator uses the revised guidelines for India.')
-st.markdown("***")
-
-st.write('**Enter height**')
-ft_selector = st.selectbox('ft', options=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-in_selector = st.selectbox('in', options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
-
-
-
 if 'clicked' not in st.session_state:
     st.session_state.clicked = False
 
@@ -18,22 +8,39 @@ if 'clicked' not in st.session_state:
 def click_button():
     st.session_state.clicked = True
 
+
+st.title('Body Mass Index Advisor for Indians 🇮🇳')
+st.write('Traditional BMI ranges do not apply to people of Indian ethnicity. This calculator uses the revised guidelines for India.')
+st.markdown("***")
+
+st.write('**Enter height**')
+ft_selector = st.selectbox('ft', options=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+in_selector = st.selectbox('in', options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+submit_height_button = st.button('Submit Height', on_click=click_button)
+
+markdown_placeholder = st.empty()
+bmi_placeholder = st.empty()
+spacer_placeholder = st.empty()
+weight_text_placeholder = st.empty()
+kg_selector_placeholder = st.empty()
+st.markdown('***')
+developer_name = st.write('Developer: Gaurav Chachra')
+
+
 if 'bmi_color_dict' not in st.session_state:
     st.session_state.bmi_color_dict = {'Underweight':'blue', 'Normal':'green', 'Overweight':'orange', 'Obese':'red'}
 
-submit_height_button = st.button('Submit Height', on_click=click_button)
 
 if st.session_state.clicked:
     height_m = (ft_selector*12 + in_selector)*2.54/100
-    st.markdown("***")
+    
 
 
     if height_m != 0:
-        text_placeholder = st.empty()
-        st.write('')
-        st.write('**Select weight**')
+        markdown_placeholder.markdown("***")
+        weight_text_placeholder.write('**Select any weight from the slider**')
         initial_weight = np.round(((21*height_m**2)*2)/2)
-        kg_selector = st.slider('kg', min_value=20., max_value=150., step=0.5, value=initial_weight, key='kg_selector')
+        kg_selector_placeholder.slider('kg', min_value=20., max_value=150., step=0.5, value=initial_weight, key='kg_selector')
         bmi = np.round(st.session_state.kg_selector/(height_m**2), 1)
 
         if bmi < 18.5:
@@ -46,5 +53,4 @@ if st.session_state.clicked:
             bmi_category = 'Obese'
 
         bmi_text_color = st.session_state.bmi_color_dict[bmi_category]
-        text_placeholder.write(f":{bmi_text_color}[**BMI: {bmi} - {bmi_category}**]")
-
+        bmi_placeholder.write(f":{bmi_text_color}[**BMI: {bmi} - {bmi_category}**]")
